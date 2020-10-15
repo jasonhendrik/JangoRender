91380523affa07149282d80f97fa8e2c443baa47
Jason Hendrik Harvey <jasonhendrik@users.noreply.github.com>
2020-10-15 18:51:51 -0400

    first version of something functional...

---
+++ JANGORENDER.ps1
@@ -0,0 +1,82 @@
+﻿Function AfterEffectsBGrendering() {
+
+#$runningInstances =	0
+
+Add-Type -AssemblyName System.Windows.Forms
+
+$FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{ 
+
+    InitialDirectory = [Environment]::GetFolderPath('Desktop') 
+
+    Filter = 'AfterEffects (*.aep, *.)|*.aep|Blender (*.blend)|*.blend'
+}
+$null = $FileBrowser.ShowDialog()
+
+$SelectedFile = Get-Content -Raw $FileBrowser.FileName
+
+$SelectedFile_simple = Get-Content $FileBrowser.FileName
+
+#regex step 1 of 2
+if ($SelectedFile -match '(?<=Ropt)(?s)(.*$)') {
+
+ $outputFolder_step1 = $Matches[1]
+
+} 
+
+#regex step 2 of 2 -  get render folder for frame count?
+if ($outputFolder_step1 -match '(?<=\"fullpath\":\")(.*)(?=\",\"platform\")') 
+{
+
+    $outputFolder = $Matches[1]
+
+    $outputFolder = $outputFolder -replace ("\\.","\")
+
+} 
+
+
+$AEproject = $FileBrowser.FileName
+
+$numOfFrames  = Read-Host 'How many frames?'
+
+$numOfInstances = Read-Host 'How many Instances?'
+
+$argsList = "-project $AEproject"
+
+$Target = "aerender"
+
+    #while this "controller-process" is running & frames arent there, KEEP GOING!
+	while ($numOfFrames -le (Get-ChildItem $outputFolder | Measure-Object ).Count) 
+    {
+        
+	    While (Get-Process JANGORENDER) 
+        {
+	
+	        #begin instances
+	        do 
+               {
+		        
+                #count process names aerender
+		        $Process = (Get-Process | Where-Object {$_.ProcessName -eq $Target} | Measure-Object).Count 
+		
+		        #if process drops out create another one
+		        if ($Process -lt $numOfInstances){
+		
+	    	    Start-Process -FilePath $Target -ArgumentList $argsList
+    	   	   }
+		
+	    }
+   
+    until($Process -eq $numOfInstances )
+		
+	}
+
+Stop-Process -processname $Target
+
+}
+
+Exit
+
+}
+
+AfterEffectsBGrendering | Out-Null
+
